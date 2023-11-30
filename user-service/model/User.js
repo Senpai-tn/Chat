d0547dc682453853284ec8a08e2ca80645c2f807
Khaled Sahli <khaledsahli36@gmail.com>
2023-11-30 23:47:46 +0100

    User model

--- user-service/model/User.js
+++ user-service/model/User.js
@@ -1,4 +1,17 @@
 const { Schema, model } = require("mongoose");
-const userSchema = new Schema({});
+const userSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  adresse: { type: String, required: false },
+  roles: { type: Array, required: true },
+  birthDate: { type: Date, required: true },
+  image: { type: String, required: false },
+  created_at: { type: Date, required: true, default: Date.now },
+  updated_at: { type: Date, required: true, default: null },
+  deleted_at: { type: Date, required: true, default: null },
+  blocked_to: { type: Date, required: true, default: null },
+});
 const User = model("users", userSchema);
 module.exports = User;
