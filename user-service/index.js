8d56eca3f3b8867b55d62a63fc7f5ce2185801de
Khaled Sahli <khaledsahli36@gmail.com>
2023-12-01 00:46:14 +0100

    Register Route

--- user-service/index.js
+++ user-service/index.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const { connect } = require("mongoose");
+const User = require("./model/User");
+const bcrypt = require("bcrypt");
+var jwt = require("jsonwebtoken");
+
+const app = express();
+app.use(express.json());
+
 connect("mongodb://127.0.0.1:27017/Chat")
   .then(() => {
     console.log("user-service connected to db");
@@ -8,11 +15,31 @@ connect("mongodb://127.0.0.1:27017/Chat")
     console.log(e);
   });
 
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("hello from user-service");
+app.post("/register", (req, res) => {
+  const { firstName, lastName, email, password, adresse, birthDate, image } =
+    req.body;
+  const user = new User({
+    firstName,
+    lastName,
+    email,
+    password: bcrypt.hashSync(password, 10),
+    adresse,
+    birthDate,
+    image,
+  });
+  user
+    .save()
+    .then((savedUser) => {
+      res.send({
+        user: savedUser,
+        token: jwt.sign({ user: savedUser }, "jwt_key"),
+      });
+    })
+    .catch((error) => {
+      res.status(500).send(error.message);
+    });
 });
+
 app.listen(5001, () => {
   console.log("User service listenning on port 5001");
 });
